feee7ce40ea5e3b79c7b2c3148e1f0186f4ccd24
Raul Torres <raultorres2603@gmail.com>
2024-09-12 12:04:58 +0200

    first commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,44 @@
+export const Login = () => {
+  const handleLogIn = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="logInComp">
+      <div className="grid grid-rows-1">
+        <div className="loginTitle my-5 text-center text-4xl font-semibold">
+          <span className="text-red-500 text-6xl">S</span>peed
+          <span className="text-red-500 text-6xl">S</span>tudy
+        </div>
+      </div>
+      <div className="formToLogIn">
+        <form onSubmit={handleLogIn}>
+          <div className="grid grid-cols-2 gap-4 mx-5">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              className="inputLogIn text-xl rounded-lg text-center"
+            />
+            <input
+              type="text"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="inputLogIn text-xl rounded-lg text-center"
+            />
+          </div>
+          <div className="grid grid-rows-1 mx-10 my-5">
+            <button
+              type="submit"
+              className="bg-red-500 transition hover:bg-red-600 duration-200 hover:scale-110 ring-red-200 ring-1"
+            >
+              Log In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,45 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
+import { Login } from "./components/Login";
+
+const getCookie = (cname: string) => {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+    loader: () => {
+      const token = getCookie("token");
+      if (token) {
+        return redirect("/");
+      }
+      return null;
+    },
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
