7308ec36c426408ff90deed0c1ec48944c89ffce
Raul Torres <raultorres2603@gmail.com>
2024-09-13 19:48:12 +0200

    middleware

--- back/routes/user.js
+++ back/routes/user.js
@@ -3,20 +3,24 @@ var router = express.Router();
 var jose = require("jose");
 
 router.get("/info", async function (req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const { payload } = await jose.jwtVerify(
+      token,
+      new TextEncoder().encode(process.env.SKT)
+    );
+    res.status(200).json({ user: payload.user });
+  } catch (error) {
+    res.status(401).json({ error: error });
+  }
+});
+
+router.use(function (req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
-  console.log(token);
   if (!token) {
     res.status(401).json({ error: "No token provided" });
   } else {
-    try {
-      const { payload } = await jose.jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.SKT)
-      );
-      res.status(200).json({ user: payload.user });
-    } catch (error) {
-      res.status(401).json({ error: error });
-    }
+    next();
   }
 });
 
